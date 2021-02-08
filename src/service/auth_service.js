b84aee1b2ebde57f794ca718474440ef675c8149
marsoon <asroq98@gmail.com>
2021-02-08 18:33:33 +0900

    add Logout

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -5,6 +5,14 @@ class AuthService{
     login(providerName){
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
         return  firebaseApp.auth().signInWithPopup(authProvider);
+    };
+    onAuthChange(onUserChanged){
+        firebase.auth().onAuthStateChanged(user =>{
+            onUserChanged(user);
+        })
+    };
+    logout(){
+        firebase.auth().signOut();
     }
 }
 
